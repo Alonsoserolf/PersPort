3aa6ca2dadf8bb58a1e8e99ec1955f4cb364a914
Alonso Gonzalez Flores <alonsoserolf@gmail.com>
2018-08-06 17:02:30 -0700

    mend

--- handler.js
+++ handler.js
@@ -32,7 +32,7 @@ module.exports.staticSiteMailer = (event, context, callback) => {
       statusCode: err ? 500: 200,
       headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'https://alonsoserolf.github.io',
+        'Access-Control-Allow-Origin': '*',
       },
       body: JSON.stringify({
         message: err ? err.message : data,
